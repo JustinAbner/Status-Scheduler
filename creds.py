6152109a85d82f54ce47194ab651324a9b14088f
JustinAbner <119388072+JustinAbner@users.noreply.github.com>
2024-09-18 23:36:18 -0400

    Create creds.py

---
+++ creds.py
@@ -0,0 +1,4 @@
+# All fields must be updated
+email = "sender@email.com" 
+password = "yourpassword" # If using gmail, an app password is required
+recipient = "receiver@email.com" # Phone number can be entered here as well, i.e. 1234567890@vtext.com is for verizion
